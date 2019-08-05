fc7864e33adbe263cf5eb237c0f33565473543d4
Pooja Suman <poojasuman212@gmail.com>
2019-08-05 14:30:48 -0700

    TreeNode class
    This class is used in all the other classes of this folder for creating and using the TreeNode

---
+++ Binary Tree/TreeNode.java
@@ -0,0 +1,17 @@
+package binaryTree;
+
+public class TreeNode {
+	
+	int data;
+	TreeNode left;
+	TreeNode right;
+	
+	TreeNode(int data)
+	{
+		this.data = data;
+		left = null;
+		right = null;
+		
+	}
+
+}
