dcbe8bdc9231ed655ea5383999dbaee712f61b46
Pooja Suman <poojasuman212@gmail.com>
2019-08-06 12:37:01 -0700

    Update terExpressionToBinTree.java

--- Binary Tree/terExpressionToBinTree.java
+++ Binary Tree/terExpressionToBinTree.java
@@ -1,3 +1,19 @@
+/*
+
+Given a string that contains ternary expressions. 
+The expressions may be nested, task is convert the given ternary expression to a binary Tree.
+
+
+Input : expression =  a?b?c:d:e
+Output :     a
+           /  \
+          b    e
+        /  \
+       c    d
+
+*/
+
+
 package binaryTree;
 
 class Node
